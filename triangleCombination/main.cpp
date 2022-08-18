ceb0fc0406fc27d3cb19355f7b2c418d2798cdf4
samuel <215023293@student.uj.ac.za>
2022-08-18 15:52:53 +0200

    added triangleCombination

---
+++ triangleCombination/main.cpp
@@ -0,0 +1,52 @@
+#include "Triangle.cpp"
+
+int main()
+{
+
+    bool cont=true;
+    char option=' ';
+    do
+    {
+        system("cls");  //clearing the console screen first
+
+        //variable to be used
+        int *ints =new int[9];
+        int sum=0;
+        Triangle tr;
+        cout<<"**************************** 4x4 magic-Triangle solver *************************"<<endl<<endl;
+        cout<<"Enter your 9 numbers : "<<endl;
+        cin>>ints[0]>>ints[1]>>ints[2]>>ints[3]>>ints[4]>>ints[5]>>ints[6]>>ints[7]>>ints[8];
+        cout<<"Enter the total :"<<endl;
+        cin>>sum;
+        cout<<"The answer is . . ."<<endl;
+        tr.setNumbers(ints,sum);
+
+        //calculating and displaying the answer
+        tr.calcAnswer();
+        tr.displayTriangle();
+
+        //asking whether the user would like to continue
+        cout<<endl;
+        cout<<"Do you wishe to continue ? [y - yes , n - no]"<<endl;
+        cin>>option;
+        while(option!='y' && option!='Y' && option!='n' && option!='N')
+        {
+            cerr<<"Invalid option . . . . please retry"<<endl;
+            cin>>option;
+        }
+        switch(option)
+        {
+        case 'y':
+        case 'Y':
+            cont=true;
+            break;
+        case 'n':
+        case 'N':
+            cout<<"Good-bey :-)"<<endl;
+            cont=false;
+            break;
+        }
+    }while(cont);
+
+    return 0;
+}
